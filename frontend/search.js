faaed82d6f8eb3ada41f902325f0db04f840d536
karan <karan@radicali.com>
2021-07-23 18:42:02 +0530

    finish search functionality

--- frontend/search.js
+++ frontend/search.js
@@ -1,33 +1,88 @@
 const icon = document.querySelector('.icon');
 const search = document.querySelector('.search');
 const esroute = 'http://localhost:3000/';
-var xhttp = new XMLHttpRequest();
+var http = new XMLHttpRequest();
 
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
+// function debounce(func, wait, immediate) {
+//   var timeout;
+//   return function () {
+//     var context = this,
+//       args = arguments;
+//     var later = function () {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
+function truncateString(str, num) {
+  if (num > str.length) {
+    return str;
+  } else {
+    str = str.substring(0, num);
+    return str + '...';
+  }
 }
 
-function success(text) {
-  console.log('hogaya');
-  console.log(text);
-}
-function getResults() {
+const getResults = () => {
   const query = document.getElementById('mysearch').value;
   const url = esroute + query;
-  console.log($.get(url));
-}
+  // const response = Object($.get(url));
+  var response;
+  var timeTaken;
+  http.open('GET', url);
+  http.send();
+  http.onreadystatechange = (e) => {
+    if (http.readyState == 4 && http.status == 200) {
+      try {
+        console.log(JSON.parse(http.responseText));
+        response = JSON.parse(http.responseText);
+        timeTaken = response['body']['took'];
+        console.log(response['body']['hits']['hits']);
+        response['body']['hits']['hits'].forEach((card) => {
+          var header = card['_source']['heading'];
+          var source = card['_source']['source'];
+          var text = truncateString(card['_source']['text'], 100);
+          document.getElementById(
+            'time'
+          ).innerHTML = `Fetched results in ${timeTaken}ms`;
+          document
+            .getElementById('searchresults')
+            .appendChild(createResultCard(header, text, source));
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  // const response = console.log(response['responseJSON']);
+  // document
+  //   .getElementById('searchresults')
+  //   .appendChild(createResultCard('1', '2'));
+};
 
-// #
-//   #
+var mysearch = document.getElementById('searchicon');
+mysearch.addEventListener('click', function () {
+  document.getElementById('searchresults').innerHTML = '';
+  getResults();
+});
+
+function createResultCard(header, text, source) {
+  var z = document.createElement('div');
+  z.innerHTML = `<div class="wiki-search-results">
+       <a href="${source}" target="_blank"> <div class='header'>
+            ${header}
+        </div></a>
+        <div class='resulttext'>
+            ${text}
+        </div>
+        <!-- <div class='source'>
+            
+        </div> -->
+        
+    </div>`;
+  return z;
+}
